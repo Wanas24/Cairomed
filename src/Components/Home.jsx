fe93fefdae11a7cdc4f93a7c3e2b28809a0075a1
Wanas24 <mwanas724@gmail.com>
2023-08-24 16:24:37 +0300

    sec commit

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -31,13 +31,13 @@ const [successMsg,setsuccessMsg]=useState('')
     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" className="" aria-label="Slide 3"></button>
   </div>
   <div className="carousel-inner">
-    <div className="carousel-item active ">
+    <div className="carousel-item active " data-bs-interval="10000">
       <img src="https://images.pexels.com/photos/2695703/pexels-photo-2695703.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" className="d-block w-100" alt="..." />
     </div>
-    <div className="carousel-item ">
+    <div className="carousel-item " data-bs-interval="10000">
       <img src="https://images.pexels.com/photos/2255459/pexels-photo-2255459.jpeg?auto=compress&cs=tinysrgb&w=600" className="d-block w-100" alt="..." />
     </div>
-    <div className="carousel-item">
+    <div className="carousel-item" data-bs-interval="10000">
       <img src="https://images.pexels.com/photos/3820303/pexels-photo-3820303.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" className="d-block w-100" alt="..." />
     </div>
   </div>
@@ -130,8 +130,11 @@ const [successMsg,setsuccessMsg]=useState('')
                
           </div>
           <br/>
-          <div class="map col-sm-12 col-lg-3">
-          <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d823.3252260891294!2d31.255484882960904!3d30.72363630362061!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2seg!4v1690032121898!5m2!1sen!2seg"  height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>       </div>
+          <div class="map col-sm-12 col-lg-3 d-flex justify-content-center align-items-center">
+  <div class="text-center mx-auto">
+    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d823.3252260891294!2d31.255484882960904!3d30.72363630362061!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2seg!4v1690032121898!5m2!1sen!2seg" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+  </div>
+</div>
      </div>
     </section>
 
