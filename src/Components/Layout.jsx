879551bc7673278f226665957a6de72ddf1a2c1f
Wanas24 <mwanas724@gmail.com>
2023-10-19 15:54:47 +0300

    sec commit

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -4,29 +4,16 @@ import Navbar from "./Navbar";
 
 const Layout = () => {
   const [isDocumentReady, setIsDocumentReady] = useState(false);
-  const [isLoadEventFired, setIsLoadEventFired] = useState(false);
 
   useEffect(() => {
-    const handleLoad = () => {
-      setIsLoadEventFired(true);
+    // Simulate document ready event
+    setTimeout(() => {
       setIsDocumentReady(true);
-    };
-
-    if (document.readyState === "complete") {
-      setIsLoadEventFired(true);
-      setIsDocumentReady(true);
-    } else {
-      window.addEventListener("load", handleLoad);
-    }
-
-    return () => {
-      window.removeEventListener("load", handleLoad);
-    };
+    }, 1500);
   }, []);
-
   return (
     <>
-      <div className={`loading-screen ${isLoadEventFired ? "fade-out" : ""}`}>
+      <div className={`loading-screen ${isDocumentReady ? "fade-out" : ""}`}>
         {isDocumentReady ? (
           <div className="content">
             <Navbar />
@@ -46,3 +33,61 @@ const Layout = () => {
 };
 
 export default Layout;
+
+
+
+
+
+
+
+
+
+
+// import React, { useEffect, useState } from "react";
+// import { Outlet } from "react-router-dom";
+// import Navbar from "./Navbar";
+
+// const Layout = () => {
+//   const [isDocumentReady, setIsDocumentReady] = useState(false);
+//   const [isLoadEventFired, setIsLoadEventFired] = useState(false);
+
+//   useEffect(() => {
+//     const handleLoad = () => {
+//       setIsLoadEventFired(true);
+//       setIsDocumentReady(true);
+//     };
+
+//     if (document.readyState === "complete") {
+//       setIsLoadEventFired(true);
+//       setIsDocumentReady(true);
+//     } else {
+//       window.addEventListener("load", handleLoad);
+//     }
+
+//     return () => {
+//       window.removeEventListener("load", handleLoad);
+//     };
+//   }, []);
+
+//   return (
+//     <>
+//       <div className={`loading-screen ${isLoadEventFired ? "fade-out" : ""}`}>
+//         {isDocumentReady ? (
+//           <div className="content">
+//             <Navbar />
+
+//             <div className="mt-5">
+//               <Outlet />
+//             </div>
+//           </div>
+//         ) : (
+//           <div className="loader">
+//             <i className="fa fa-spinner fa-spin fa-5x text-white"></i>
+//           </div>
+//         )}
+//       </div>
+//     </>
+//   );
+// };
+
+// export default Layout;
